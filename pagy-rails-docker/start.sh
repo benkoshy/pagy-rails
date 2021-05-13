ebc8f372443ba61cfcb1319d5db9cf1666d154fb
Domizio Demichelis <dd.nexus@gmail.com>
2021-05-13 14:17:25 +0700

    added rerun for easy development

--- pagy-rails-docker/start.sh
+++ pagy-rails-docker/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/dumb-init bash
 
 ./bin/webpack-dev-server &
-./bin/rails server -b 0.0.0.0
+rerun --wait 2 -- ./bin/rails server -b 0.0.0.0
 
 # just keep the container running if we shoot down the default servers above
 tail -f /dev/null
