9012758a901a421693423cb25ab211c3274564bd
Domizio Demichelis <dd.nexus@gmail.com>
2021-11-23 15:04:53 +0700

    Added seeds for calendar, updated pagy

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,30 @@
 
 rating = %i[poor fair good excellent outstanding]
 
-1.upto(5000).each do |i|
-  Movie.create(created_at: i.months.ago, name: "#{FFaker::Movie.title} - #{i}", rating: rating[rand(5)].to_sym)
+start  = DateTime.new(2018, 11, 18)
+finish = DateTime.new(2021, 3, 12)
+gap1   = [DateTime.new(2019, 3, 14),  DateTime.new(2019, 5, 3)]
+gap2   = [DateTime.new(2019, 12, 20), DateTime.new(2020, 2, 14)]
+gap3   = [DateTime.new(2020, 5, 23),  DateTime.new(2020, 7, 8)]
+
+current = start
+while current <= finish  do
+  next if rand(6).zero?                             # 1/6 additional random empty days
+  attributes = []
+  rand(1..15).times do                              # records per day
+    time = current + rand(1.day.to_i).seconds       # random time of day
+    attributes << { name: "#{FFaker::Movie.title}",
+                    rating: rating[rand(5)].to_sym,
+                    created_at: time,
+                    updated_at: time }
+  end
+  current = case current
+            when gap1.first then gap1.last          # skip to end of gap
+            when gap2.first then gap2.last          # skip to end of gap
+            when gap3.first then gap3.last          # skip to end of gap
+            else current + 1.day                    # next day
+            end
+  Movie.insert_all(attributes)
+  print '.'
 end
+puts
