b3a0b3cd32f59bf2fa2e449e15811b63fb4e4250
BK <benkoshy@hotmail.com>
2021-12-15 16:05:12 +1100

    add: calendar for to the no styles view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,9 +3,19 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    # @pagy, @movies = pagy(Movie.all.order(:rating))
-    @pagy, @movies = pagy(Movie.where(rating: %i[poor good outstanding]).order(:rating))
+    movie_collection = Movie.where(rating: %i[poor good outstanding]).order(:rating)
+
+    if params[:calendar]
+      @calendar, @pagy, @movies = pagy_calendar(movie_collection, year:   { size: [1, 1, 1, 1] },
+                                                month:  { size: [0, 12, 12, 0], format: '%b' },
+                                                pagy:   { items: 10 },
+                                                active: params[:calendar])
+
+    else
+      @pagy, @movies = pagy(movie_collection)
+    end
   end
+
 
   # GET /movies/1 or /movies/1.json
   def show
@@ -67,4 +77,16 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie).permit(:name, :rating)
   end
+
+  private
+
+  def pagy_calendar_period(collection)
+    minmax = collection.pluck('MIN(created_at)', 'MAX(created_at)').first
+    minmax.map { |time| Time.parse(time).in_time_zone.to_time }
+  end
+
+  def pagy_calendar_filter(collection, from, to)
+    collection.where(created_at: from...to)  # 3-dots range excluding the end value
+  end
+
 end
