231994e3cc88aff7c931078994ba81eae9c4d342
BK <benkoshy@hotmail.com>
2023-02-27 14:34:37 +1100

    upgrade: rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_27_033013) do
+ActiveRecord::Schema[6.1].define(version: 2023_02_27_033013) do
 
   create_table "alt_movies", force: :cascade do |t|
     t.string "name"
