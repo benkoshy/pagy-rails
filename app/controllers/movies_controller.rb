469b8f91174affae300eb928864cdf9a58d926e0
BK <benkoshy@hotmail.com>
2023-03-06 10:26:10 +1100

    fix: refactor - move common actions to the application controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -78,15 +78,4 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:name, :rating)
   end
 
-  private
-
-  def pagy_calendar_period(collection)
-    minmax = collection.pluck('MIN(created_at)', 'MAX(created_at)').first
-    minmax.map { |time| Time.parse(time).in_time_zone.to_time }
-  end
-
-  def pagy_calendar_filter(collection, from, to)
-    collection.where(created_at: from...to)  # 3-dots range excluding the end value
-  end
-
 end
