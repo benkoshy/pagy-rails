ccd83ce70f63995478319d147a2ccfb5f41ae413
BK <benkoshy@hotmail.com>
2023-03-23 11:00:48 +1100

    wip: add ransack search using turbo_streams (there is a bug here atm)

--- app/controllers/bootstrap_movies_controller.rb
+++ app/controllers/bootstrap_movies_controller.rb
@@ -10,5 +10,10 @@ class BootstrapMoviesController < ApplicationController
 	def search
 		@q = Movie.ransack(params[:q])
 		@pagy, @movies = pagy(@q.result(distinct: true), items: 2)
+
+		respond_to do |format|
+			format.html
+			format.turbo_stream {render "search"}
+		end
 	end
 end
